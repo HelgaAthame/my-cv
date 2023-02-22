04827eae6f83c6b95b6750ea15f85d7fa9317518
HelgaAthame <athamethedarkest@gmail.com>
2023-02-22 18:59:09 +0300

    init: start project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Controller } from "./controller/index.js";
+import { View } from "./view/index.js";
+
+class App {
+  view: View;
+  controller: Controller;
+  constructor (view: View, controller: Controller) {
+    this.view = view;
+    this.controller = controller;
+  }
+}
+
+const app = new App(new View(), new Controller());
+
+app.view.renderUI();
+
+//export { app };
