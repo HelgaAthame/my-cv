b5554b9a26c82e219349147195b56250ae1d6c15
HelgaAthame <athamethedarkest@gmail.com>
2023-02-24 15:56:05 +0300

    feat: add education links

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -11,6 +11,7 @@ export class Controller {
 
   addListeners() {
     this.projectLinks();
+    this.educationLinks();
   }
 
   projectLinks() {
@@ -38,5 +39,21 @@ export class Controller {
     asyncRace.addEventListener('click', () => window.open('https://github.com/rolling-scopes-school/helgaathame-JSFE2022Q3/pull/45', '_blank'));
     newsApi.addEventListener('click', () => window.open('https://github.com/rolling-scopes-school/helgaathame-JSFE2022Q3/pull/43', '_blank'));
 
+  }
+
+  educationLinks() {
+    const seoCourse = selectorChecker(document, '.seo-course');
+    const nodeJSCourse = selectorChecker(document, '.node-js-course');
+    const javascriptCourse = selectorChecker(document, '.javascript-course');
+    const academyOfScience = selectorChecker(document, '.academy-of-science');
+    const reactCourse = selectorChecker(document, '.react-course');
+    const performanceOptimization = selectorChecker(document, '.performance-optimization-course');
+
+    seoCourse.addEventListener('click', () => window.open('https://proseo.by/kursy/', '_blank'));
+    nodeJSCourse.addEventListener('click', () => window.open('https://rs.school/nodejs/', '_blank'));
+    javascriptCourse.addEventListener('click', () => window.open('https://rs.school/js/', '_blank'));
+    academyOfScience.addEventListener('click', () => window.open('https://www.ipnk.basnet.by/', '_blank'));
+    reactCourse.addEventListener('click', () => window.open('https://rs.school/react/', '_blank'));
+    performanceOptimization.addEventListener('click', () => window.open('https://training.by/', '_blank'));
   }
 }
