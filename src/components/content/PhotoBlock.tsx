af00241a41b483ee66356f35db1ae8aacb30db51
HelgaAthame <athamethedarkest@gmail.com>
2024-10-25 16:48:58 +0300

    feat: enhance shadows

--- src/components/content/PhotoBlock.tsx
+++ src/components/content/PhotoBlock.tsx
@@ -12,16 +12,16 @@ export const PhotoBlock = () => {
       onClick={() => {
         setShow((prev) => !prev);
       }}
-      className="info__wrapper w-40 h-40 rounded-full duration-1000 p-2 border-solid
+      className="info__wrapper w-40 h-40 rounded-full duration-500 p-2 border-solid
       border-4 border-sky-200 hover:margin-neg-3rem relative hover:shadow-glow
       hover:width-12rem hover:height-12rem group
       cursor-pointer"
     >
       <div
         className="info__photo max-w-full max-h-full bg-red-200 overflow-hidden 
-        rounded-full grayscale-[50%] opacity-40 contrast-200 hover:filter-none duration-200
-        hover:width-12rem hover:height-12rem hover:shadow-smallglow hover:bg-size-12rem
-        hover:opacity-80"
+        rounded-full grayscale-[50%] opacity-40 contrast-200 group-hover:filter-none duration-500
+        hover:width-12rem hover:height-12rem hover:bg-size-12rem
+        group-hover:opacity-80"
       >
         <Image src={myPhoto} alt={"my photo"} width={160} height={160} />
       </div>
@@ -31,7 +31,7 @@ export const PhotoBlock = () => {
             data-tooltip="+375(29)601-71-88"
             className="text-4xl w-16 h-16 border-sky-200 flex items-center justify-center
             rounded-full border-4 border-solid with-tooltip phone-icon absolute 
-            -top-6 -left-14 overflow-hidden hover:shadow-smallglow"
+            -top-6 -left-14 overflow-hidden hover:shadow-glow"
           >
             📞
           </div>
@@ -39,14 +39,14 @@ export const PhotoBlock = () => {
             data-tooltip="athamethedarkset@gmail.com"
             className="text-4xl w-16 h-16 border-sky-200 flex items-center justify-center 
             rounded-full border-4 border-solid -top-6 -right-14 absolute with-tooltip
-             main-icon overflow-hidden hover:shadow-smallglow"
+             main-icon overflow-hidden hover:shadow-glow"
           >
             ✉
           </div>
           <div
             className="text-4xl w-16 h-16 border-sky-200 flex items-center justify-center 
             rounded-full border-4 border-solid -bottom-6 -right-14 absolute linkedin-icon
-             overflow-hidden hover:shadow-smallglow"
+             overflow-hidden hover:shadow-glow"
           >
             <Image
               src={linkedin}
@@ -58,7 +58,7 @@ export const PhotoBlock = () => {
           <div
             className="text-4xl w-16 h-16 border-sky-200 flex items-center justify-center 
             rounded-full border-4 border-solid -bottom-6 -left-14 absolute github-icon
-             overflow-hidden hover:shadow-smallglow"
+             overflow-hidden hover:shadow-glow"
           >
             <Image src={github} alt={"gihub icon"} width={50} height={50} />
           </div>
